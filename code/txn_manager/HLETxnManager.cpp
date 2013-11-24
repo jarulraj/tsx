bd0fff3b775350ba2b41be6f4b993128556bc854
Joy Arulraj <jarulraj@cs.cmu.edu>
2013-11-23 22:32:17 -0500

    Removed per-key locking for now

--- code/txn_manager/HLETxnManager.cpp
+++ code/txn_manager/HLETxnManager.cpp
@@ -24,53 +24,12 @@ HLETxnManager::HLETxnManager(std::unordered_map<long,std::string> *table)
 bool HLETxnManager::RunTxn(const std::vector<OpDescription> &operations,
         std::vector<string> *get_results) {
 
-    /*
-    // Construct an ordered set of keys to lock.
-    set<uint64_t> keys;
-    for (const OpDescription &op : operations) {
-        keys.insert(op.key);
-    }
-    
-    // Lock keys in order.
-    for (uint64_t key : keys) {
-        hle_spinlock_acquire(&table_lock);
-        if (lockTable.count(key) == 0) {
-            spinlock_t lock ;
-            lock.v = 0;
-
-            lockTable[key] = &lock;
-            
-            hle_spinlock_release(&table_lock);
-
-            hle_spinlock_acquire(&lock);
-        } else {
-            spinlock_t* lockPtr = lockTable[key];
-
-            hle_spinlock_release(&table_lock);
-
-            hle_spinlock_acquire(lockPtr);
-        }
-    }
-    */
-
     hle_spinlock_acquire(&table_lock);
 
     // Do transaction.
     ExecuteTxnOps(operations, get_results);
 
     hle_spinlock_release(&table_lock);
-
-    /*
-    // Unlock all keys in reverse order.
-    std::set<uint64_t>::reverse_iterator rit;
-    for (rit = keys.rbegin(); rit != keys.rend(); ++rit) {
-        hle_spinlock_acquire(&table_lock);
-        
-        hle_spinlock_release(lockTable[*rit]);
-        
-        hle_spinlock_release(&table_lock);
-    }
-    */
 
     return true;
 }
