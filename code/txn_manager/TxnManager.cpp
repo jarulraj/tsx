4c6555c820b06fd8f2df3247ef9b64597e3bb856
Joy Arulraj <jarulraj@cs.cmu.edu>
2013-12-08 00:58:27 -0500

    Removed txn logic emulation

--- code/txn_manager/TxnManager.cpp
+++ code/txn_manager/TxnManager.cpp
@@ -11,8 +11,6 @@
 using namespace std;
 using namespace std::chrono;
 
-#define OP_COUNT 1000000
-
 bool TxnManager::ExecuteTxnOps(const vector<OpDescription> &operations,
         std::vector<string> *get_results) {
     string result;
@@ -24,22 +22,6 @@ bool TxnManager::ExecuteTxnOps(const vector<OpDescription> &operations,
             ExecuteTxnOp(op);
         }
     }
-
-    // SIMULATING TEMPORAL OVERHEAD OF APPLICATION LOGIC
-    /*high_resolution_clock::time_point t1 = high_resolution_clock::now();
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(1));
-
-    int dummy_ops = OP_COUNT;
-    int val = rand() % 10;
-    for(int op_itr = 0 ; op_itr++ ; op_itr < dummy_ops) {
-      cout << "fdsa";
-        val *= 2; 
-    }
-
-    high_resolution_clock::time_point t2 = high_resolution_clock::now();
-    nanoseconds gap = duration_cast<nanoseconds>(t2 - t1);*/
-    //cout<<"Gap :"<<gap.count()<<" ns"<<endl;
 
     return true;
 }
