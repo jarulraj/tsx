547275d1dd4329e615db98a0b2a7cf6e0afb4919
Joy Arulraj <jarulraj@cs.cmu.edu>
2013-12-09 11:38:08 -0500

    Added dynamic rx sets to LockTable

--- code/txn_manager/LockTableTxnManager.cpp
+++ code/txn_manager/LockTableTxnManager.cpp
@@ -14,27 +14,99 @@
 
 using namespace std;
 
+const int MAX_TRIES = 10;
 
 bool LockTableTxnManager::RunTxn(const std::vector<OpDescription> &operations,
         std::vector<string> *get_results, ThreadStats *stats) {
+    if (dynamic) {
+        bool abort = true;
+        string result;
 
-    // Construct an ordered set of keys to lock.
-    set<long> keys;
-    for (const OpDescription &op : operations) {
-        keys.insert(op.key);
-    }
+        while (abort) {
+            set<long> keys;
+            unordered_map<long, string> old_values;
+            abort = false;
+            
+            for (const OpDescription &op : operations) {
+                if (keys.count(op.key) == 0) {                    
+		    mutex *m = &lockTable[op.key];
+		    int tries = 0;
+		    auto start = std::chrono::high_resolution_clock::now();
 
-    // Lock keys in order.
-    for (long key : keys) {
-      TIME_CODE(stats, lockTable[key].lock());
-    }
+		    while (!m->try_lock()) {
+			auto end = std::chrono::high_resolution_clock::now();
+			stats->lock_acq_time += end - start;
 
-    // Do transaction.
-    ExecuteTxnOps(operations, get_results);
+			tries++;
+			if (tries == MAX_TRIES) {
+			    abort = true;
+			    if (get_results != NULL) {
+				get_results->clear();
+			    }
+			    break;
+			}
 
-    // Unlock all keys in reverse order.
-    for (auto rit = keys.rbegin(); rit != keys.rend(); ++rit) {
-        lockTable[*rit].unlock();
+			// Prepare timing info for next iteration.
+			start = std::chrono::high_resolution_clock::now();
+		    }
+
+		    auto end = std::chrono::high_resolution_clock::now();
+		    stats->lock_acq_time += end - start;
+
+		    if (abort) {
+			break;
+		    }
+                
+                    keys.insert(op.key);
+                }
+
+                // Run this op.
+                ExecuteTxnOp(op, &result);
+                
+                if (op.type == GET) {
+                    if (get_results != NULL) {
+                        get_results->push_back(result);
+                    }
+                } else if (old_values.count(op.key) == 0) {
+                    old_values[op.key] = result;
+                }
+            }
+
+            // Roll back changes if we aborted.
+            if (abort) {
+                OpDescription op;
+                op.type = INSERT;
+                for (auto it = old_values.begin(); it != old_values.end(); it++) {
+                    op.key = it->first;
+                    op.value = it->second;
+                    ExecuteTxnOp(op);
+                }
+            }
+
+            // Unlock all keys in reverse order.
+            for (auto rit = keys.rbegin(); rit != keys.rend(); ++rit) {
+                lockTable[*rit].unlock();
+            }
+        }
+    } else {    
+	// Construct an ordered set of keys to lock.
+	set<long> keys;
+	for (const OpDescription &op : operations) {
+	    keys.insert(op.key);
+	}
+
+	// Lock keys in order.
+	for (long key : keys) {
+	    TIME_CODE(stats, lockTable[key].lock());
+	}
+
+	// Do transaction.
+	ExecuteTxnOps(operations, get_results);
+
+	// Unlock all keys in reverse order.
+	for (auto rit = keys.rbegin(); rit != keys.rend(); ++rit) {
+	    lockTable[*rit].unlock();
+	}
     }
 
     return true;
