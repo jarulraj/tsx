0d737b9c85dec8c36c2f9578c7cf16c06c13375b
Joy Arulraj <jarulraj@cs.cmu.edu>
2013-12-09 06:08:32 -0500

    Final experiments for graph generation

---
+++ code/results.sh
@@ -0,0 +1,11 @@
+# FINAL EXPERIMENTS
+
+python experiment.py  --k zipf --t 4 --o 64 --i 3 --y 2,4,8,16,32
+python experiment.py  --k zipf --t 4 --o 8  --i 3 --y 1,2,4,8
+
+python experiment.py  --k zipf --t 2,4,8 --o 64  --i 3 --y 4
+
+# DYNAMIC --enable in script
+# python experiment.py  --k zipf --t 4 --o 64 --i 3 --y 2,4,8,16,32
+
+
